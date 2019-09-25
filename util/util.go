accfc5cf8202b84502f38b03431e254fb5e203d8
Dmitry Chepurovskiy <dm3ch@dm3ch.net>
2019-09-25 16:29:23 +0300

    Fix demo to work with env vars

--- util/util.go
+++ util/util.go
@@ -2,27 +2,20 @@ package util
 
 import (
 	"io"
+	"log"
 
 	opentracing "github.com/opentracing/opentracing-go"
-	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
 	"github.com/uber/jaeger-lib/metrics"
 )
 
 func InitTracer() (io.Closer, error) {
-	//tracer initiallization
-	// Sample configuration for testing. Use constant sampling to sample every trace
-	// and enable LogSpan to log every span via configured Logger.
-	cfg := jaegercfg.Configuration{
-		ServiceName: "myservice",
-		Sampler: &jaegercfg.SamplerConfig{
-			Type:  jaeger.SamplerTypeConst,
-			Param: 1,
-		},
-		Reporter: &jaegercfg.ReporterConfig{
-			LogSpans: true,
-		},
+	log.Print("Init start")
+	cfg, err := jaegercfg.FromEnv()
+
+	if err != nil {
+		return nil, err
 	}
 	// Example logger and metrics factory. Use github.com/uber/jaeger-client-go/log
 	// and github.com/uber/jaeger-lib/metrics respectively to bind to real logging and metrics
@@ -37,6 +30,6 @@ func InitTracer() (io.Closer, error) {
 	)
 
 	opentracing.SetGlobalTracer(tracer)
-
+	log.Print("Init finished")
 	return closer, err
 }
